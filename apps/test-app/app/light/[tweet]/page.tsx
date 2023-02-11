11ba088c9f1b3820c8c5eaa4ffbec7c370e94904
LFades <luis@vercel.com>
2023-02-11 16:01:22 -0500

    Fix build error

--- apps/test-app/app/light/[tweet]/page.tsx
+++ apps/test-app/app/light/[tweet]/page.tsx
@@ -6,6 +6,7 @@ import TweetPage from './tweet-page'
 export default async function Page({ params }: { params: { tweet: string } }) {
   return (
     <Suspense fallback={<TweetSkeleton />}>
+      {/* @ts-ignore: Async components are valid in the app directory */}
       <TweetPage id={params.tweet} className={theme.light} />
     </Suspense>
   )
