a9aed8ba2d26e734b561f6acbb5d0c1720617f03
LFades <luis@vercel.com>
2023-02-11 16:28:32 -0500

    Set next.js to canary and apply workaround

--- apps/test-app/app/light/[tweet]/page.tsx
+++ apps/test-app/app/light/[tweet]/page.tsx
@@ -3,7 +3,20 @@ import { TweetSkeleton } from 'next-tweet'
 import theme from 'next-tweet/theme.module.css'
 import TweetPage from './tweet-page'
 
-export default async function Page({ params }: { params: { tweet: string } }) {
+export const revalidate = 60
+
+export async function generateStaticParams() {
+  return []
+}
+
+export default async function Page({
+  params,
+  ...props
+}: {
+  params: { tweet: string }
+}) {
+  // TODO: Figure out why Next.js sends this value at build time
+  if (params.tweet === '%5Btweet%5D') return null
   return (
     <Suspense fallback={<TweetSkeleton />}>
       {/* @ts-ignore: Async components are valid in the app directory */}
