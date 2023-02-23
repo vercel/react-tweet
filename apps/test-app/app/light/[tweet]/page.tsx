6949954cd39fd9c9022c547585049077a96a2012
LFades <luis@vercel.com>
2023-02-23 10:53:46 -0500

    use metadata

--- apps/test-app/app/light/[tweet]/page.tsx
+++ apps/test-app/app/light/[tweet]/page.tsx
@@ -1,8 +1,28 @@
 import { NextTweet } from 'next-tweet'
+import { getTweet } from 'next-tweet/api'
 
-export const revalidate = 60
+type Props = {
+  params: { tweet: string }
+}
 
-export default async function Page({ params }: { params: { tweet: string } }) {
+export const revalidate = 1800
+
+export async function generateMetadata({ params }: Props) {
+  const tweet = await getTweet(params.tweet).catch(() => undefined)
+
+  if (!tweet) return { title: 'Next Tweet' }
+
+  const username = ` - @${tweet.user.screen_name}`
+  const maxLength = 68 - username.length
+  const text =
+    tweet.text.length > maxLength
+      ? `${tweet.text.slice(0, maxLength)}…`
+      : tweet.text
+
+  return { title: `${text}${username}` }
+}
+
+export default async function Page({ params }: Props) {
   // TODO: Figure out why Next.js sends this value at build time
   if (params.tweet === '%5Btweet%5D') return null
 
