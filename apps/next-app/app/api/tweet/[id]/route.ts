27a0c3c2bcc5605da3b72319618fda7fe783dca4
LFades <luis@vercel.com>
2023-03-10 01:50:03 -0500

    Updated endpoint

--- apps/next-app/app/api/tweet/[id]/route.ts
+++ apps/next-app/app/api/tweet/[id]/route.ts
@@ -1,14 +1,15 @@
 import { NextResponse } from 'next/server'
 import { getTweet } from 'next-tweet/api'
+import cors from 'edge-cors'
 
 type RouteSegment = { params: { id: string } }
 
-export async function GET(_req: Request, { params }: RouteSegment) {
+export async function GET(req: Request, { params }: RouteSegment) {
   let tweet
   try {
     tweet = await getTweet(params.id)
   } catch (error) {
     console.error(error)
   }
-  return NextResponse.json({ data: tweet ?? null })
+  return cors(req, NextResponse.json({ data: tweet ?? null }))
 }
