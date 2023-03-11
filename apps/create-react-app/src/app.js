63f88d2aa2e38f580ede012d2ee7507ed628f6d4
LFades <luis@vercel.com>
2023-03-10 21:48:24 -0500

    Removed unrequired theme prop

--- apps/create-react-app/src/app.js
+++ apps/create-react-app/src/app.js
@@ -5,15 +5,13 @@ import './base.css'
 
 export default function App() {
   return (
-    <div data-theme="dark">
-      <div className={clsx(styles.root, 'react-tweet-theme')}>
-        <main className={styles.main}>
-          <Tweet id="1628832338187636740" />
-        </main>
-        <footer className={styles.footer}>
-          <p>🤯 This tweet was statically generated.</p>
-        </footer>
-      </div>
+    <div className={clsx(styles.root, 'react-tweet-theme')}>
+      <main className={styles.main}>
+        <Tweet id="1628832338187636740" />
+      </main>
+      <footer className={styles.footer}>
+        <p>🤯 This tweet was statically generated.</p>
+      </footer>
     </div>
   )
 }
