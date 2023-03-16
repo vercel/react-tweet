148e721b0300fc7fc331bcb0e43639b6d044e63c
LFades <luis@vercel.com>
2023-03-16 14:30:59 -0500

    Update verified icon and tweet header text

--- packages/react-tweet/src/tweet-header.tsx
+++ packages/react-tweet/src/tweet-header.tsx
@@ -41,33 +41,28 @@ export const TweetHeader = ({ tweet, components }: Props) => {
           rel="noopener noreferrer"
         >
           <div className={s.authorLinkText}>
-            <span className={s.name} title={tweet.user.name}>
-              {tweet.user.name}
-            </span>
+            <span title={tweet.user.name}>{tweet.user.name}</span>
           </div>
-          {tweet.user.verified && (
-            <div
-              className={clsx(
-                s.authorVerified,
-                // For some reason `is_blue_verified` is always false, so we use the existence
-                // of `verified_type` to determine if the account is blue verified.
-                // `verified_type` can be something like "government", a blue user doesn't have it.
-                tweet.user.verified &&
-                  (tweet.user.verified_type ? s.verifiedOld : s.verifiedBlue)
-              )}
-            >
-              <svg
-                viewBox="0 0 24 24"
-                aria-label="Verified account"
-                role="img"
-                className={s.authorVerifiedIcon}
+          {tweet.user.verified ||
+            (tweet.user.is_blue_verified && (
+              <div
+                className={clsx(
+                  s.authorVerified,
+                  tweet.user.is_blue_verified ? s.verifiedBlue : s.verifiedOld
+                )}
               >
-                <g>
-                  <path d="M22.25 12c0-1.43-.88-2.67-2.19-3.34.46-1.39.2-2.9-.81-3.91s-2.52-1.27-3.91-.81c-.66-1.31-1.91-2.19-3.34-2.19s-2.67.88-3.33 2.19c-1.4-.46-2.91-.2-3.92.81s-1.26 2.52-.8 3.91c-1.31.67-2.2 1.91-2.2 3.34s.89 2.67 2.2 3.34c-.46 1.39-.21 2.9.8 3.91s2.52 1.26 3.91.81c.67 1.31 1.91 2.19 3.34 2.19s2.68-.88 3.34-2.19c1.39.45 2.9.2 3.91-.81s1.27-2.52.81-3.91c1.31-.67 2.19-1.91 2.19-3.34zm-11.71 4.2L6.8 12.46l1.41-1.42 2.26 2.26 4.8-5.23 1.47 1.36-6.2 6.77z"></path>
-                </g>
-              </svg>
-            </div>
-          )}
+                <svg
+                  viewBox="0 0 24 24"
+                  aria-label="Verified account"
+                  role="img"
+                  className={s.authorVerifiedIcon}
+                >
+                  <g>
+                    <path d="M22.25 12c0-1.43-.88-2.67-2.19-3.34.46-1.39.2-2.9-.81-3.91s-2.52-1.27-3.91-.81c-.66-1.31-1.91-2.19-3.34-2.19s-2.67.88-3.33 2.19c-1.4-.46-2.91-.2-3.92.81s-1.26 2.52-.8 3.91c-1.31.67-2.2 1.91-2.2 3.34s.89 2.67 2.2 3.34c-.46 1.39-.21 2.9.8 3.91s2.52 1.26 3.91.81c.67 1.31 1.91 2.19 3.34 2.19s2.68-.88 3.34-2.19c1.39.45 2.9.2 3.91-.81s1.27-2.52.81-3.91c1.31-.67 2.19-1.91 2.19-3.34zm-11.71 4.2L6.8 12.46l1.41-1.42 2.26 2.26 4.8-5.23 1.47 1.36-6.2 6.77z"></path>
+                  </g>
+                </svg>
+              </div>
+            ))}
         </a>
         <div className={s.authorMeta}>
           <a
