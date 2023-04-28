3a7fbb8bf0a9f106ca1af2913ed3996865998eb4
Ty Carlson <tywayne@gmail.com>
2023-04-28 12:22:17 -0500

    derive value in component instead of util function

--- packages/react-tweet/src/tweet-replies.tsx
+++ packages/react-tweet/src/tweet-replies.tsx
@@ -1,18 +1,27 @@
+import { useMemo } from 'react'
 import type { Tweet } from './api/index.js'
-import { getTweetUrl, getRepliesLinkText } from './utils.js'
+import { getTweetUrl, formatNumber } from './utils.js'
 import s from './tweet-replies.module.css'
 
-export const TweetReplies = ({ tweet }: { tweet: Tweet }) => (
-  <div className={s.replies}>
-    <a
-      className={s.link}
-      href={getTweetUrl(tweet)}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <span className={s.text}>
-        {getRepliesLinkText(tweet.conversation_count)}
-      </span>
-    </a>
-  </div>
-)
+export const TweetReplies = ({ tweet }: { tweet: Tweet }) => {
+  const repliesLinkText = useMemo(() => {
+    if (tweet.conversation_count === 0) return 'Read more on Twitter'
+
+    return tweet.conversation_count === 1
+      ? `Read ${formatNumber(tweet.conversation_count)} reply`
+      : `Read ${formatNumber(tweet.conversation_count)} replies`
+  }, [tweet.conversation_count])
+
+  return (
+    <div className={s.replies}>
+      <a
+        className={s.link}
+        href={getTweetUrl(tweet)}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <span className={s.text}>{repliesLinkText}</span>
+      </a>
+    </div>
+  )
+}
