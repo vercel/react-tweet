b1866f9cb15d25743fdb7275d8b3d43c409ae1ab
Ty Carlson <tywayne@gmail.com>
2023-05-17 14:49:16 -0500

    Fix link text grammar in TweetReplies component (#77)
    * fix plural grammar in TweetReplies component
    
    * derive value in component instead of util function

--- packages/react-tweet/src/tweet-replies.tsx
+++ packages/react-tweet/src/tweet-replies.tsx
@@ -1,20 +1,27 @@
+import { useMemo } from 'react'
 import type { Tweet } from './api/index.js'
 import { getTweetUrl, formatNumber } from './utils.js'
 import s from './tweet-replies.module.css'
 
-export const TweetReplies = ({ tweet }: { tweet: Tweet }) => (
-  <div className={s.replies}>
-    <a
-      className={s.link}
-      href={getTweetUrl(tweet)}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <span className={s.text}>
-        {tweet.conversation_count > 0
-          ? `Read ${formatNumber(tweet.conversation_count)} replies`
-          : 'Read more on Twitter'}
-      </span>
-    </a>
-  </div>
-)
+export const TweetReplies = ({ tweet }: { tweet: Tweet }) => {
+  const repliesLinkText = useMemo(() => {
+    if (tweet.conversation_count === 0) return 'Read more on Twitter'
+
+    return tweet.conversation_count === 1
+      ? `Read ${formatNumber(tweet.conversation_count)} reply`
+      : `Read ${formatNumber(tweet.conversation_count)} replies`
+  }, [tweet.conversation_count])
+
+  return (
+    <div className={s.replies}>
+      <a
+        className={s.link}
+        href={getTweetUrl(tweet)}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <span className={s.text}>{repliesLinkText}</span>
+      </a>
+    </div>
+  )
+}
