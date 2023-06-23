c3da331d8d81910ee505dea4907c485d15107bf5
Luis Alvarez <luis@vercel.com>
2023-06-23 14:39:48 -0500

    Removed unrequired use of useMemo

--- packages/react-tweet/src/tweet-replies.tsx
+++ packages/react-tweet/src/tweet-replies.tsx
@@ -1,16 +1,14 @@
-import { useMemo } from 'react'
 import type { Tweet } from './api/index.js'
 import { getTweetUrl, formatNumber } from './utils.js'
 import s from './tweet-replies.module.css'
 
 export const TweetReplies = ({ tweet }: { tweet: Tweet }) => {
-  const repliesLinkText = useMemo(() => {
-    if (tweet.conversation_count === 0) return 'Read more on Twitter'
-
-    return tweet.conversation_count === 1
+  const repliesLinkText =
+    tweet.conversation_count === 0
+      ? 'Read more on Twitter'
+      : tweet.conversation_count === 1
       ? `Read ${formatNumber(tweet.conversation_count)} reply`
       : `Read ${formatNumber(tweet.conversation_count)} replies`
-  }, [tweet.conversation_count])
 
   return (
     <div className={s.replies}>
