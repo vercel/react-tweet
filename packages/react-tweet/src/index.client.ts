7a233d8e0bca012e15c32fc55c20eb080eaca5c7
Luis Alvarez <luis@vercel.com>
2023-06-23 22:24:20 -0500

    Export hooks and utils

--- packages/react-tweet/src/index.client.ts
+++ packages/react-tweet/src/index.client.ts
@@ -1,2 +1,4 @@
 export * from './twitter-theme/components.js'
 export * from './swr.js'
+export * from './utils.js'
+export * from './hooks.js'
--- packages/react-tweet/src/index.ts
+++ packages/react-tweet/src/index.ts
@@ -2,3 +2,5 @@
 // can be useful for anyone that wans to do more deep edits in the default theme.
 export * from './twitter-theme/components.js'
 export * from './tweet.js'
+export * from './utils.js'
+export * from './hooks.js'
