30e8774552e2e0da42ffc60e50a7ed5ea5b534f0
Luis Alvarez <luis@vercel.com>
2023-07-10 18:18:48 -0500

    Added missing extensions

--- packages/react-tweet/src/twitter-theme/icons/index.ts
+++ packages/react-tweet/src/twitter-theme/icons/index.ts
@@ -1,3 +1,3 @@
-export * from './verified'
-export * from './verified-business'
-export * from './verified-government'
+export * from './verified.js'
+export * from './verified-business.js'
+export * from './verified-government.js'
