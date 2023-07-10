bdbde0cf03cdb6371d75f3c16a02ddddc0c9e0c0
Luis Alvarez <luis@vercel.com>
2023-07-10 18:02:22 -0500

    Handle the new verified icons

--- packages/react-tweet/src/twitter-theme/tweet-header.tsx
+++ packages/react-tweet/src/twitter-theme/tweet-header.tsx
@@ -2,6 +2,11 @@ import clsx from 'clsx'
 import type { EnrichedTweet } from '../utils.js'
 import type { TwitterComponents } from './types.js'
 import { AvatarImg } from './avatar-img.js'
+import {
+  Verified,
+  VerifiedGovernment,
+  VerifiedBusiness,
+} from './icons/index.js'
 import s from './tweet-header.module.css'
 
 type Props = {
@@ -11,7 +16,28 @@ type Props = {
 
 export const TweetHeader = ({ tweet, components }: Props) => {
   const Img = components?.AvatarImg ?? AvatarImg
+  const { user } = tweet
+  const verified = user.verified || user.is_blue_verified || user.verified_type
+  let icon = <Verified />
+  let iconClassName: string | null = s.verifiedBlue
 
+  if (verified) {
+    if (!user.is_blue_verified) {
+      iconClassName = s.verifiedOld
+    }
+    switch (user.verified_type) {
+      case 'Government':
+        icon = <VerifiedGovernment />
+        iconClassName = s.verifiedGovernment
+        break
+      case 'Business':
+        icon = <VerifiedBusiness />
+        iconClassName = null
+        break
+    }
+  }
+
+  console.log(tweet.user)
   return (
     <div className={s.header}>
       <a
@@ -20,7 +46,12 @@ export const TweetHeader = ({ tweet, components }: Props) => {
         target="_blank"
         rel="noopener noreferrer"
       >
-        <div className={s.avatarOverflow}>
+        <div
+          className={clsx(
+            s.avatarOverflow,
+            tweet.user.profile_image_shape === 'Square' && s.avatarSquare
+          )}
+        >
           <Img
             src={tweet.user.profile_image_url_https}
             alt={tweet.user.name}
@@ -42,26 +73,9 @@ export const TweetHeader = ({ tweet, components }: Props) => {
           <div className={s.authorLinkText}>
             <span title={tweet.user.name}>{tweet.user.name}</span>
           </div>
-          {tweet.user.verified ||
-            (tweet.user.is_blue_verified && (
-              <div
-                className={clsx(
-                  s.authorVerified,
-                  tweet.user.is_blue_verified ? s.verifiedBlue : s.verifiedOld
-                )}
-              >
-                <svg
-                  viewBox="0 0 24 24"
-                  aria-label="Verified account"
-                  role="img"
-                  className={s.authorVerifiedIcon}
-                >
-                  <g>
-                    <path d="M22.25 12c0-1.43-.88-2.67-2.19-3.34.46-1.39.2-2.9-.81-3.91s-2.52-1.27-3.91-.81c-.66-1.31-1.91-2.19-3.34-2.19s-2.67.88-3.33 2.19c-1.4-.46-2.91-.2-3.92.81s-1.26 2.52-.8 3.91c-1.31.67-2.2 1.91-2.2 3.34s.89 2.67 2.2 3.34c-.46 1.39-.21 2.9.8 3.91s2.52 1.26 3.91.81c.67 1.31 1.91 2.19 3.34 2.19s2.68-.88 3.34-2.19c1.39.45 2.9.2 3.91-.81s1.27-2.52.81-3.91c1.31-.67 2.19-1.91 2.19-3.34zm-11.71 4.2L6.8 12.46l1.41-1.42 2.26 2.26 4.8-5.23 1.47 1.36-6.2 6.77z"></path>
-                  </g>
-                </svg>
-              </div>
-            ))}
+          {verified && (
+            <div className={clsx(s.authorVerified, iconClassName)}>{icon}</div>
+          )}
         </a>
         <div className={s.authorMeta}>
           <a
