64c89e73325d1318100f0c00235327b299b486db
Ty Carlson <tywayne@gmail.com>
2023-07-21 22:57:50 -0500

    Fixes regression in media layout with multiple images

--- packages/react-tweet/src/twitter-theme/tweet-media.tsx
+++ packages/react-tweet/src/twitter-theme/tweet-media.tsx
@@ -1,10 +1,19 @@
 import { Fragment } from 'react'
 import clsx from 'clsx'
 import { type EnrichedTweet, getMediaUrl } from '../utils.js'
+import { MediaDetails } from '../api/index.js'
 import type { TwitterComponents } from './types.js'
 import { TweetMediaVideo } from './tweet-media-video.js'
 import { MediaImg } from './media-img.js'
 import s from './tweet-media.module.css'
+
+const getSkeletonStyle = (media: MediaDetails) => ({
+  width: media.type === 'video' ? 'unset' : undefined,
+  paddingBottom: `${Math.max(
+    (100 / media.original_info.width) * media.original_info.height,
+    56.25
+  )}%`,
+})
 
 type Props = {
   tweet: EnrichedTweet
@@ -17,29 +26,16 @@ export const TweetMedia = ({ tweet, components }: Props) => {
 
   return (
     <div className={s.root}>
-      {tweet.mediaDetails?.map((media) => (
-        <Fragment key={media.media_url_https}>
-          <div
-            className={s.skeleton}
-            style={
-              media.type === 'photo'
-                ? {
-                    paddingBottom: `${
-                      (100 / media.original_info.width) *
-                      media.original_info.height
-                    }%`,
-                  }
-                : undefined
-            }
-          />
-          <div
-            className={clsx(
-              s.mediaWrapper,
-              length > 1 && s.grid2Columns,
-              length === 3 && s.grid3,
-              length > 4 && s.grid2x2
-            )}
-          >
+      <div
+        className={clsx(
+          s.mediaWrapper,
+          length > 1 && s.grid2Columns,
+          length === 3 && s.grid3,
+          length > 4 && s.grid2x2
+        )}
+      >
+        {tweet.mediaDetails?.map((media) => (
+          <Fragment key={media.media_url_https}>
             {media.type === 'photo' ? (
               <a
                 key={media.media_url_https}
@@ -48,6 +44,7 @@ export const TweetMedia = ({ tweet, components }: Props) => {
                 target="_blank"
                 rel="noopener noreferrer"
               >
+                <div className={s.skeleton} style={getSkeletonStyle(media)} />
                 <Img
                   src={getMediaUrl(media, 'small')}
                   alt={media.ext_alt_text || 'Image'}
@@ -57,12 +54,13 @@ export const TweetMedia = ({ tweet, components }: Props) => {
               </a>
             ) : (
               <div key={media.media_url_https} className={s.mediaContainer}>
+                <div className={s.skeleton} style={getSkeletonStyle(media)} />
                 <TweetMediaVideo tweet={tweet} media={media} />
               </div>
             )}
-          </div>
-        </Fragment>
-      ))}
+          </Fragment>
+        ))}
+      </div>
     </div>
   )
 }
