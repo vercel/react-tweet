8b313848c3686f72bbede5fa5e9f160370e277a0
Luis Alvarez <luis@vercel.com>
2023-08-14 17:25:27 -0500

    Added token

--- packages/react-tweet/src/api/get-tweet.ts
+++ packages/react-tweet/src/api/get-tweet.ts
@@ -24,6 +24,12 @@ export class TwitterApiError extends Error {
 
 const TWEET_ID = /^[0-9]+$/
 
+function getToken(id: string) {
+  return ((Number(id) / 1e15) * Math.PI)
+    .toString(6 ** 2)
+    .replace(/(0+|\.)/g, '')
+}
+
 /**
  * Fetches a tweet from the Twitter syndication API.
  */
@@ -46,8 +52,11 @@ export async function getTweet(
       'tfw_follower_count_sunset:true',
       'tfw_tweet_edit_backend:on',
       'tfw_refsrc_session:on',
+      'tfw_fosnr_soft_interventions_enabled:on',
+      'tfw_show_birdwatch_pivots_enabled:on',
       'tfw_show_business_verified_badge:on',
       'tfw_duplicate_scribes_to_settings:on',
+      'tfw_use_profile_image_shape_enabled:on',
       'tfw_show_blue_verified_badge:on',
       'tfw_legacy_timeline_sunset:true',
       'tfw_show_gov_verified_badge:on',
@@ -55,7 +64,8 @@ export async function getTweet(
       'tfw_tweet_edit_frontend:on',
     ].join(';')
   )
-  
+  url.searchParams.set('token', getToken(id))
+
   const res = await fetch(url.toString(), fetchOptions)
   const isJson = res.headers.get('content-type')?.includes('application/json')
   const data = isJson ? await res.json() : undefined
