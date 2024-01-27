ad6ad9029b22bfb62208a8d0f2d0bf020a124904
Mohammed Al-Rozzi <irozzi@icloud.com>
2024-01-27 09:20:47 +0000

    testing

--- apps/create-react-app/src/index.js
+++ apps/create-react-app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import App from './app';
-import App2 from './app2';
+import App from './app.js';
+import App2 from './app2.js';
 
 ReactDOM.render(
   <React.StrictMode>
