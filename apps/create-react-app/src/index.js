e30d5bdfdb00fc42615489bbeecd1b7224ffbda2
Mohammed Al-Rozzi <irozzi@icloud.com>
2024-01-27 09:32:28 +0000

    test

--- apps/create-react-app/src/index.js
+++ apps/create-react-app/src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './app.js';
 import App2 from './app2.js';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <Switch>
-        <Route exact path="/" component={App} />
-        <Route path="/app2" component={App2} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/app2" element={<App2 />} />
+      </Routes>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
