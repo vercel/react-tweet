133d497abb836cccefb77586b7ce3c64468d4c73
Mohammed Al-Rozzi <irozzi@icloud.com>
2024-01-27 09:17:34 +0000

    test

--- apps/create-react-app/src/index.js
+++ apps/create-react-app/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import App from 'App';
 import App from './app';
 import App2 from './app2';
 
