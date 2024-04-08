89209174e95aa53dd67446bbbe9d1dadfa233f5e
Luis Alvarez <luis@vercel.com>
2024-04-08 11:18:41 -0500

    Make the date a server component

--- packages/react-tweet/src/twitter-theme/tweet-info-created-at.tsx
+++ packages/react-tweet/src/twitter-theme/tweet-info-created-at.tsx
@@ -1,20 +1,11 @@
-'use client'
-
 import type { EnrichedTweet } from '../utils.js'
-import { useMounted } from '../hooks.js'
-import s from './tweet-info-created-at.module.css'
 import { formatDate } from '../date-utils.js'
+import s from './tweet-info-created-at.module.css'
 
 export const TweetInfoCreatedAt = ({ tweet }: { tweet: EnrichedTweet }) => {
-  const mounted = useMounted()
-  // If the date is displayed immediately, it will produce a server/client mismatch because the date
-  // format will change depending on the user's browser. If the format were to be simplified to
-  // something like "MMM d, y", then you could use the server date.
-  const createdAt =
-    typeof window !== 'undefined' && mounted ? new Date(tweet.created_at) : null
-  if (!createdAt) return null
-
+  const createdAt = new Date(tweet.created_at)
   const formattedCreatedAtDate = formatDate(createdAt)
+
   return (
     <a
       className={s.root}
