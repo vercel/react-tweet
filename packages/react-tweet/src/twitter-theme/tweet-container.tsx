0d3c2d53e176efbd15f716f3081a6ca82cb96469
Thereallo <thereallo1026@gmail.com>
2025-10-24 06:34:35 -0400

    fix: remove automatic CSS import for Next.js 16 Turbopack compatibility

--- packages/react-tweet/src/twitter-theme/tweet-container.tsx
+++ packages/react-tweet/src/twitter-theme/tweet-container.tsx
@@ -1,12 +1,11 @@
-import type { ReactNode } from 'react'
-import clsx from 'clsx'
-import s from './tweet-container.module.css'
-import './theme.css'
+import clsx from "clsx";
+import type { ReactNode } from "react";
+import s from "./tweet-container.module.css";
 
-type Props = { className?: string; children: ReactNode }
+type Props = { className?: string; children: ReactNode };
 
 export const TweetContainer = ({ className, children }: Props) => (
-  <div className={clsx('react-tweet-theme', s.root, className)}>
-    <article className={s.article}>{children}</article>
-  </div>
-)
+	<div className={clsx("react-tweet-theme", s.root, className)}>
+		<article className={s.article}>{children}</article>
+	</div>
+);
