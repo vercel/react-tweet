89aacae86591ed71e0842c506ad61576b1ed2237
Adrien Zaganelli <adrienzaganelli@gmail.com>
2026-01-28 13:40:38 +0100

    Use react-tweet exports in vue-better-tweet core package (#1)
    * Use react-tweet exports for vue-better-tweet core
    
    * Copy twitter theme CSS modules

---
+++ packages/vue-better-tweet/src/index.ts
@@ -0,0 +1,9 @@
+export {
+  enrichTweet,
+  formatDate,
+  formatNumber,
+  getMediaUrl,
+  getMp4Video,
+  getMp4Videos,
+} from 'react-tweet'
+export type { EnrichedQuotedTweet, EnrichedTweet, TweetCoreProps } from 'react-tweet'
