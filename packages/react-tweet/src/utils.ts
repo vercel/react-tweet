031c92f81b6ff6f98eb4336ad647922644644e08
James Singleton <jamesrsingleton1@gmail.com>
2026-05-22 07:44:54 -0700

    fix(entities): safeguard entities not being defined

--- packages/react-tweet/src/utils.ts
+++ packages/react-tweet/src/utils.ts
@@ -13,8 +13,6 @@ import type {
   MediaVideo,
 } from './api/index.js'
 
-export  { formatDate } from './date-utils.js'
-
 export type TweetCoreProps = {
   id: string
   onError?(error: any): any
@@ -50,7 +48,7 @@ const getInReplyToUrl = (tweet: Tweet) =>
 
 export const getMediaUrl = (
   media: MediaDetails,
-  size: 'small' | 'medium' | 'large'
+  size: 'small' | 'medium' | 'large',
 ): string => {
   const url = new URL(media.media_url_https)
   const extension = url.pathname.split('.').pop()
@@ -122,12 +120,13 @@ function getEntities(tweet: TweetBase): Entity[] {
     { indices: tweet.display_text_range, type: 'text' },
   ]
 
-  addEntities(result, 'hashtag', tweet.entities.hashtags)
-  addEntities(result, 'mention', tweet.entities.user_mentions)
-  addEntities(result, 'url', tweet.entities.urls)
-  addEntities(result, 'symbol', tweet.entities.symbols)
-  if (tweet.entities.media) {
-    addEntities(result, 'media', tweet.entities.media)
+  const { entities } = tweet
+  if (entities) {
+    addEntities(result, 'hashtag', entities.hashtags)
+    addEntities(result, 'mention', entities.user_mentions)
+    addEntities(result, 'url', entities.urls)
+    addEntities(result, 'symbol', entities.symbols)
+    addEntities(result, 'media', entities.media)
   }
   fixRange(tweet, result)
 
@@ -158,8 +157,10 @@ function getEntities(tweet: TweetBase): Entity[] {
 function addEntities(
   result: EntityWithType[],
   type: EntityWithType['type'],
-  entities: TweetEntity[]
+  entities?: TweetEntity[],
 ) {
+  if (!entities?.length) return
+
   for (const entity of entities) {
     for (const [i, item] of result.entries()) {
       if (
@@ -195,11 +196,9 @@ function addEntities(
  * Array.from is unicode aware, unlike string.slice()
  */
 function fixRange(tweet: TweetBase, entities: EntityWithType[]) {
-  if (
-    tweet.entities.media &&
-    tweet.entities.media[0].indices[0] < tweet.display_text_range[1]
-  ) {
-    tweet.display_text_range[1] = tweet.entities.media[0].indices[0]
+  const media = tweet.entities?.media
+  if (media && media[0].indices[0] < tweet.display_text_range[1]) {
+    tweet.display_text_range[1] = media[0].indices[0]
   }
   const lastEntity = entities.at(-1)
   if (lastEntity && lastEntity.indices[1] > tweet.display_text_range[1]) {
