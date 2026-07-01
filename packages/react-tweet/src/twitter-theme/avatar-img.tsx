c103376ef9fc914dac37a0229184595c1a334f7e
highoncomputers <highoncomputers@users.noreply.github.com>
2026-07-01 03:30:43 +0000

    fix: replace _normal avatar URL suffix to prevent 404s
    Twitter's CDN often purges _normal-suffixed profile images, causing
    avatars in embedded tweets to 404. Replace the _normal suffix with
    _400x400, which uses a more stable CDN asset path.
    
    Closes #201
    
    Co-authored-by: BankkRoll <BankkRoll@users.noreply.github.com>

--- packages/react-tweet/src/twitter-theme/avatar-img.tsx
+++ packages/react-tweet/src/twitter-theme/avatar-img.tsx
@@ -5,5 +5,12 @@ type AvatarImgProps = {
   height: number
 }
 
+// Replace Twitter CDN URL size suffix to avoid 404s on stale assets
+function normalizeAvatarSrc(src: string): string {
+  return src.replace(/(\/profile_images\/.+)_normal(\.\w+)$/, '$1_400x400$2')
+}
+
 // eslint-disable-next-line jsx-a11y/alt-text -- The alt text is part of `...props`
-export const AvatarImg = (props: AvatarImgProps) => <img {...props} />
+export const AvatarImg = (props: AvatarImgProps) => (
+  <img {...props} src={normalizeAvatarSrc(props.src)} />
+)
